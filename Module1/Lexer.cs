c82b8ae46b363d88a56292dbb7d48bd194650941
unknown <user@mil8a-202a-10.mmcs.sfedu.ru>
2019-09-06 21:33:24 +0300

    Task 1

--- Module1/Lexer.cs
+++ Module1/Lexer.cs
@@ -58,8 +58,9 @@ namespace Lexer
 
         protected System.Text.StringBuilder intString;
         public int parseResult = 0;
+		Boolean isNegative = false;
 
-        public IntLexer(string input)
+		public IntLexer(string input)
             : base(input)
         {
             intString = new System.Text.StringBuilder();
@@ -70,12 +71,17 @@ namespace Lexer
             NextCh();
             if (currentCh == '+' || currentCh == '-')
             {
+				if (currentCh == '-')
+				{
+					isNegative = true;
+				}
                 NextCh();
             }
         
             if (char.IsDigit(currentCh))
             {
-                NextCh();
+				parseResult = parseResult *10 + int.Parse(currentCh.ToString());
+				NextCh();
             }
             else
             {
@@ -84,7 +90,8 @@ namespace Lexer
 
             while (char.IsDigit(currentCh))
             {
-                NextCh();
+				parseResult = parseResult * 10 + int.Parse(currentCh.ToString());
+				NextCh();
             }
 
 
@@ -93,8 +100,13 @@ namespace Lexer
                 Error();
             }
 
-            return true;
+			if (isNegative == true)
+			{
+				parseResult *= -1;
+			}
 
+			return true;
+			
         }
     }
     
